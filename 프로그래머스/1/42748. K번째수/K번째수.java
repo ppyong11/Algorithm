639a6135252cf6d7635ed13acf76d6378c77692a
아현 <kah1722291@gmail.com>
2025-10-08 00:28:46 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,17 +3,11 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length]; //2차원 배열 행 개수로 길이 지정
-        int answerIdx= 0;
         
-        for(int[] idxArray:commands){ //2차원 -> 1차원
-            int[] tmp= new int[idxArray[1]-idxArray[0]+1];
-            int idx= 0;
-            
-            for(int i=idxArray[0]; i <= idxArray[1]; i++){ //해당하는 idx의 요소 담기
-                tmp[idx++]= array[i-1];
-            }
+        for(int i= 0; i < commands.length; i++){
+            int[] tmp= Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
             Arrays.sort(tmp); //오름차순 정렬
-            answer[answerIdx++]= tmp[idxArray[2]-1];
+            answer[i]= tmp[commands[i][2]-1];
         }
         return answer;
     }
