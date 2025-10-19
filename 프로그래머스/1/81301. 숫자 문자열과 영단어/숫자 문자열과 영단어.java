c36b660021664b5fd2ffd8ad6608c911dd9050ba
아현 <kah1722291@gmail.com>
2025-10-20 01:39:29 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.16 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,35 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        Map map= Map.of(
-            "zero", "0",
-            "one", "1",
-            "two", "2",
-            "three", "3",
-            "four", "4",
-            "five", "5",
-            "six", "6",
-            "seven", "7",
-            "eight", "8",
-            "nine", "9"
-        );
-        
-        StringBuilder answer= new StringBuilder(); //정답
-        StringBuilder tmp= new StringBuilder();
-        
-        for(char ch:s.toCharArray()){
-            if(Character.isDigit(ch)){
-                answer.append(ch);
-                continue;
-            }
-            tmp.append(ch); //StringBuilder에 ch 저장 가능
-            if(map.containsKey(tmp.toString())){
-                answer.append(map.get(tmp.toString()));
-                tmp.setLength(0); //비우기
-            }
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i)); //Integer to String
         }
-        System.out.printf("%s", tmp.toString());
-        return Integer.parseInt(answer.toString());
+        return Integer.parseInt(s); //String to int
     }
 }
